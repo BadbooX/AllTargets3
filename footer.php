f7afaa24740e6c8e70184ff7720c4ab0a2397cf4
BadbooX <k.cotton.pro@gmail.com>
2022-06-08 21:52:42 +0200

    Footer new

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="bg-dark text-white ">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                <h5 class="text-uppercase  font-weight-bold text-white">Liens utiles</h5>
+                <p>
+                    <a href="index.php" id="linksfoot" class="text-white">Accueil<br></a>
+                    <a href="contact.php" id="linksfoot" class="text-white">Connexion<br></a>
+                    <a href="contact.php" id="linksfoot" class="text-white">Contact<br></a>
+                </p>
+            </div>
+            
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto ">
+                <h5 class="text-uppercase  font-weight-bold text-white">Contact</h5>
+                <p>
+                    <i class="fa-solid fa-house mr-3 text-white"></i> Habite chez soi
+                </p>
+                <p>
+                    <i class="fa-solid fa-phone mr-3 text-white"></i> 06 ** ** ** ** **
+                </p>
+                
+                
+            </div>
+        </div>
+       
+        <div class="row align-items-center text-align-center">
+            <div class="col-md-7 ml-7 col-lg-8">
+                <p class="copyright">Copyright &copy 2022 Tous droits réservés par: Kévin Cotton
+                </p>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <ul class="list-unstyled list-inline">
+                    <!-- github -->
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>AllTargets - Accueil</title>
 </head>
+<body>
+        
     <?php 
         include "navbar.php";
     ?>
@@ -29,7 +31,8 @@
             </div>
         </div>
     </div>
-<body>
+    <?php include "footer.php" ?>
+
     <script src="https://kit.fontawesome.com/b6728b60f5.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
