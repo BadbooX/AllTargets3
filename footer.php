81cf8a207bf511407bbbb4d19e21e3270da8d906
BadbooX <k.cotton.pro@gmail.com>
2022-06-09 09:14:12 +0200

    Commit modif footer, nav, index

--- footer.php
+++ footer.php
@@ -7,26 +7,24 @@
                     <a href="index.php" id="linksfoot" class="text-white">Accueil<br></a>
                     <a href="contact.php" id="linksfoot" class="text-white">Connexion<br></a>
                     <a href="contact.php" id="linksfoot" class="text-white">Contact<br></a>
-                    <a href=""><i class="logogit fa-brands fa-github text-white"></i></a>
+                    <a href=""><i class="logogit fa-brands fa-github"></i></a>
                 </p>
             </div>
             
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto ">
                 <h5 class="footertitle text-uppercase  font-weight-bold">Contact</h5>
-                <p>
-                    <i class="fa-solid fa-house mr-3 text-white"></i> Habite chez soi
+                <p class="text-white">
+                    <i class="footertitle fa-solid fa-house mr-3"></i> 31 Avenue de mézières
                 </p>
-                <p>
-                    <i class="fa-solid fa-phone mr-3 text-white"></i> 06 ** ** ** ** **
+                <p class="text-white">
+                    <i class="footertitle fa-solid fa-phone mr-3 "></i> 06/74/26/54/15
                 </p>
-                
-                
             </div>
         </div>
        
         <div class="row d-flex justify-content-center align-items-center text-align-center">
             <div class="col-md-7 ml-7 col-lg-8">
-                <p class="copyright">Copyright &copy 2022 Tous droits réservés par: Kévin Cotton
+                <p class="copyright text-white">Copyright &copy 2022 Tous droits réservés par: Kévin Cotton
                 </p>
             </div>
             
