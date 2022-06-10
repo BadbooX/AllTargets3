95ed82891998d7d5481b862cb6920db5527aa2d9
BadbooX <k.cotton.pro@gmail.com>
2022-06-10 08:13:26 +0200

    landing + système verif session

--- connexionTraitement.php
+++ connexionTraitement.php
@@ -6,19 +6,24 @@
 
         $pseudo = htmlspecialchars($_POST['pseudo']);
         $password = htmlspecialchars($_POST['password']);
+        
 
-        $check = $bdd->prepare('SELECT identifiant_user, psswd_user FROM alltargets WHERE identifiant_user=?');
+        $check = $bdd->prepare('SELECT identifiant_user, psswd_user FROM user_alltargets WHERE identifiant_user=?');
         $check->execute(array($pseudo));
         $data = $check->fetch();
         $row = $check->rowCount();
+     
         if($row == 1)
         {
             if($data['identifiant_user'] === $pseudo)
             {
                 $password = hash('md5', $password); 
+                
                 if($data['psswd_user'] === $password)
                 {
                     $_SESSION['user'] = $data['pseudo'];
+                    $_SESSION['logged'] = true;
+                    
                     header('Location:landing.php');
 
                 }else header('Location:connexion.php?login_err=mdp');
