761e0a5f79e5dd849a56b50beb0d0dda796cae0c
BadbooX <k.cotton.pro@gmail.com>
2022-06-12 09:56:20 +0200

    commit formish

--- formishuser_traitement.php
+++ formishuser_traitement.php
@@ -15,12 +15,17 @@ if(isset($_POST['nameProjet']) && !empty($_POST['nameProjet'])
     $categoProj = htmlspecialchars($_POST['categoProjet']);
 
 
-    $check = $bdd->prepare('INSERT INTO `projetuser_alltargets`(`id_projetuser`, `nom_projetuser`, `lien`, `image_projetuser`, `time_projetuser`, `nomcreateur_projetuser`, `categorie_projetuser`) VALUES(?,?,?,?,?,?,?)');
-    $check->execute(array());
-    header('Location:landing.php');
+    $sql = $bdd->prepare('INSERT INTO `projetuser_alltargets`(`nom_projetuser`, `lien`, `image_projetuser`,`nomcreateur_projetuser`, `categorie_projetuser`) VALUES(?,?,?,?,?)');
+    $sql->bindParam(1,$nameProj);
+    $sql->bindParam(2,$linkProj);
+    $sql->bindParam(3,$linkPicProj);
+    $sql->bindParam(4,$PseudoProj);
+    $sql->bindParam(5,$categoProj);
+    $sql->execute();
+    header('Location:landing.php?insert_err=succes');
 
 }else{
-    header('Location:landing.php');
+    header('Location:landing.php?insert_err=fail');
 }
 
 ?>
