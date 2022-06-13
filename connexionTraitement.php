3908ae03fdfa9802586f3ca345407ac3adf222ec
BadbooX <k.cotton.pro@gmail.com>
2022-06-13 10:28:34 +0200

    commit start admin

--- connexionTraitement.php
+++ connexionTraitement.php
@@ -9,7 +9,7 @@
         $password = htmlspecialchars($_POST['password']);
         
 
-        $check = $bdd->prepare('SELECT identifiant_user, psswd_user FROM user_alltargets WHERE identifiant_user=?');
+        $check = $bdd->prepare('SELECT identifiant_user, psswd_user, isAdmin FROM user_alltargets WHERE identifiant_user=?');
         $check->execute(array($pseudo));
         $data = $check->fetch();
         $row = $check->rowCount();
@@ -22,12 +22,22 @@
                 
                 if($data['psswd_user'] === $password)
                 {
-                    session_start();
-                    $_SESSION['pseudo'] = $pseudo;
+                    if($data['isAdmin'] != 0){
+                        session_start();
+                        $_SESSION['pseudo'] = $pseudo;
+                        $_SESSION['role'] = 1;
+                        $_SESSION['logged'] = true;
+                        header('Location:landingAdmin.php');
+                        
+                    }else{
+                        session_start();
+                        $_SESSION['pseudo'] = $pseudo;
+                        $_SESSION['role'] = 0;
+                        $_SESSION['logged'] = true;
+    
+                        header('Location:landing.php');
+                    }
                     
-                    $_SESSION['logged'] = true;
-
-                    header('Location:landing.php');
 
                 }else header('Location:connexion.php?login_err=mdp');
             }else header('Location:connexion.php?login_err=identifiant');
